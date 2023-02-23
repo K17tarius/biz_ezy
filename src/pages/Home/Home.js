f3aa6cc0f3ac8e16f70f77b72402aa62af19c5f9
K17tarius <raghav.juyal@science.christuniversity.in>
2023-02-23 22:42:53 +0530

    carousel

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,32 @@
 import "./Home.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import CarouselComponent from "../../components/Carousel/Carousel";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <Container>
+      <Row className="mt-4">
+        <Col md="5">
+          <div className="bannerDiv">
+            <div>
+              <div className="bannerDivMainText">
+                Clearance Sale! Get upto 50% off
+              </div>
+              <div className="bannerDivSubText">
+                We have lowered our prices.
+              </div>
+              <div className="bannerDivButton">Shop Now</div>
+            </div>
+          </div>
+        </Col>
+        <Col md="7">
+          <CarouselComponent></CarouselComponent>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default Home;
